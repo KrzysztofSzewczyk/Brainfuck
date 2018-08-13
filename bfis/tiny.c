ea376ceff37814105ab8c3772705ff0e797a5d5e
Palaiologos <marekszuwarek1958@gmail.com>
2018-08-13 14:55:43 +0200

    Create tiny.c
    Original location: https://gist.github.com/KrzysztofSzewczyk/1daf14100005b61b5d1693fb483d35ff

---
+++ bfis/tiny.c
@@ -0,0 +1,11 @@
+#define f(x)if(i[t]==x)
+#define u unsigned
+#define y char
+u y l[6000]={0};int main(){y*c="+-"
+"<>].,[";b y*t=",[.,]";u y*p=l;y 
+r;int i=0,k;for(;i[t];i++){p+=i[t]
+==c[3]?1:(i[t]==c[2]?-1:0);*p+=i[t]==c[0]
+?1:(i[t]==c[1]?-1:0);f(c[5])putchar(*p);
+f(c[11])*p=getchar();if(c[4]&&(k=*p)){z:
+if((k+=t[--i]==c[7]?-1:(i[t]==c[4]?1:0))>0)
+goto z;}}}
